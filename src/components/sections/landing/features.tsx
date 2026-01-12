cf40fc762a08f29c3e2d1d59cc1a0b035e735939
Prince Shah <Prince.rjb839@gmail.com>
2026-01-12 23:40:07 +0530

    add: prettier-tailwind-plugin for formating and aranging tailwind classes

--- src/components/sections/landing/features.tsx
+++ src/components/sections/landing/features.tsx
@@ -3,15 +3,15 @@ import { FEATURESDATA } from "~/constants";
 
 export function FeatureSection() {
   return (
-    <section className="w-full flex flex-col items-center">
-      <div className="w-full h-12 border-b border-border/80 flex items-center justify-center">
-        <div className="w-full md:w-5/6 h-12 border-x border-border/80 flex items-center justify-center">
-          <h2 className="text-[13px] tracking-wider text-muted-foreground font-space-grotesk">
+    <section className="flex w-full flex-col items-center">
+      <div className="border-border/80 flex h-12 w-full items-center justify-center border-b">
+        <div className="border-border/80 flex h-12 w-full items-center justify-center border-x md:w-5/6">
+          <h2 className="text-muted-foreground font-space-grotesk text-[13px] tracking-wider">
             Explore Our Features
           </h2>
         </div>
       </div>
-      <div className="w-full md:w-5/6 divide-y md:divide-x divide-border/80 flex-1 border-x border-border/80 grid grid-cols-1 md:grid-cols-3">
+      <div className="divide-border/80 border-border/80 grid w-full flex-1 grid-cols-1 divide-y border-x md:w-5/6 md:grid-cols-3 md:divide-x md:divide-y-0">
         {FEATURESDATA.map((item, idx) => (
           <FeatureCard key={item.icon} {...item} />
         ))}
